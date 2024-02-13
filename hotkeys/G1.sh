dd9dc2681ebf4eb25d6757a52fddaf702a25a0e6
Chris Chase <chris@accede.com.au>
2024-02-13 10:40:12 +1030

    Added G* hot keys

---
+++ hotkeys/G1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/chris/workbox/AccedeTools/20minrule/20mins.sh
---
+++ hotkeys/G2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/remmina -c /home/chris/.local/share/remmina/office_rdp_surface_192-168-15-3.remmina
