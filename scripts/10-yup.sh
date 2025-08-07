5df4402cb9a3f41616a7183a684bc516e03dd76b
chris <chris@homeass.sirmonkeys.com>
2025-08-07 12:57:33 +0930

    ASD

--- scripts/10-yup.sh
+++ scripts/10-yup.sh
@@ -1,8 +1,6 @@
-:#!/bin/bash
-
-echo "-- Docker Containers --"
-echo "----------------------------------------------------------------------------------------------"
-
+#!/bin/sh
+#
+echo ""
 docker ps --format '{{.Names}}|{{.Image}}|{{.Status}}' | while IFS="|" read -r name image status; do
     printf "📌 %-30s %-40s %-20s\n" "$name" "$image" "$status"
 done
