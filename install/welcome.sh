6a12aa88f40b6356f86790a820e0b6600707890e
Chris Chase <sirmedia@sirmonkeys.com>
2025-08-07 00:37:58 +0000

    removed unwanted welcomes

--- install/welcome.sh
+++ install/welcome.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #
 
-
-
-TARGET="/etc/update-motd.d/10-yup"
+MOTD_DIR="/etc/update-motd.d"
+CUSTOM_SCRIPT="$MOTD_DIR/10-yup"
 SOURCE="/home/chris/sirenv/scripts/10-yup.sh"
 
-if [ ! -e "$TARGET" ]; then
-    sudo ln -s "$SOURCE" "$TARGET"
+# Remove unwanted MOTD components
+sudo rm -f "$MOTD_DIR/50-motd-news" "$MOTD_DIR/10-help-text"
+
+# Create symlink if missing
+if [ ! -e "$CUSTOM_SCRIPT" ]; then
+    sudo ln -s "$SOURCE" "$CUSTOM_SCRIPT"
 fi
