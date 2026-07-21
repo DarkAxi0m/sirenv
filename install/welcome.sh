3654f1d8112ce6374177998a4fd803a2b4895d84
chris <chris@PiHole.sirmonkeys.com>
2026-07-21 12:00:58 +0930

    welcome

--- install/welcome.sh
+++ install/welcome.sh
@@ -6,9 +6,9 @@ CUSTOM_SCRIPT="$MOTD_DIR/10-yup"
 SOURCE="/home/chris/sirenv/scripts/10-yup.sh"
 
 # Remove unwanted MOTD components
-sudo rm -f "$MOTD_DIR/50-motd-news" "$MOTD_DIR/10-help-text"
+ rm -f "$MOTD_DIR/50-motd-news" "$MOTD_DIR/10-help-text"
 
 # Create symlink if missing
 if [ ! -e "$CUSTOM_SCRIPT" ]; then
-    sudo ln -s "$SOURCE" "$CUSTOM_SCRIPT"
+    ln -s "$SOURCE" "$CUSTOM_SCRIPT"
 fi
